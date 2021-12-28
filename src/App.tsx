22ca67534e273f9ef218fba9c0ea9a9405002023
kishorshivsharan0055 <kishorshivsharan>
2021-12-28 11:22:54 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  DataSearch,
+  MultiList,
+  ReactiveBase,
+  ReactiveList,
+  ResultCard,
+} from "@appbaseio/reactivesearch";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ReactiveBase
+      url="https://KheuWOeu6:63aa1cd7-5628-425c-90d7-6680efea1e55@scary-machine-qjxpseq-arc.searchbase.io"
+      app="test-appp"
+      credentials="KheuWOeu6:63aa1cd7-5628-425c-90d7-6680efea1e55"
+      enableAppbase
+    >
+      <Navbar />
+      {/* other components will go here. */}
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "30%",
+            margin: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MultiList
+            componentId="genresfilter"
+            dataField="genres.keyword"
+            className="h-10"
+            title="Filter by genres"
+            aggregationSize={5}
+            showCheckbox
+          />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", width: "66%" }}>
+          <DataSearch
+            style={{
+              marginTop: "35px",
+            }}
+            componentId="searchbox"
+            dataField={[
+              "genres",
+              "genres.autosuggest",
+              "original_title",
+              "original_title.autosuggest",
+            ]}
+            fieldWeights={[3, 1, 5, 1]}
+            placeholder="Search for genres or movie"
+          />
+          <ReactiveList
+            componentId="results"
+            dataField="name"
+            size={6}
+            pagination={true}
+            react={{
+              and: ["searchbox", "ratingsfilter", "genresfilter"],
+            }}
+            style={{ textAlign: "center" }}
+            render={({ data }) => (
+              <ReactiveList.ResultCardsWrapper>
+                {data.map((item: any) => (
+                  <ResultCard key={item._id}>
+                    <ResultCard.Image
+                      style={{
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${item.poster_path})`,
+                      }}
+                      src=""
+                    />
+                    <ResultCard.Title
+                      dangerouslySetInnerHTML={{
+                        __html: item.original_title,
+                      }}
+                    />
+                  </ResultCard>
+                ))}
+              </ReactiveList.ResultCardsWrapper>
+            )}
+          />
+        </div>
+      </div>
+    </ReactiveBase>
   );
 }
 
